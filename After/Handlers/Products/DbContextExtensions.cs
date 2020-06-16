7b7846197aecbb3079ce089bbbc6e86ce2db207a
Denis Tsvettsikh <denis_tsvettsikh@epam.com>
2020-06-16 16:32:02 +0700

    Extentension method to load an aggregate

---
+++ After/Handlers/Products/DbContextExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Handlers.Products
+{
+    public static class DbContextExtensions
+    {
+        public static Task<Product> FindFullAsync(this IQueryable<Product> dbSet, int id)
+        {
+            return dbSet
+                .Include(x => x.ProductCategories)
+                .ThenInclude(x => x.Category)
+                .SingleOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
