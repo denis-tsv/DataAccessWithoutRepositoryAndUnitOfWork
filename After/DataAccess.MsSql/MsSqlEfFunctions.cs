debcaaf0a060eaf3b15c6983f5cd71d723a7f799
Denis Tsvettsikh <denis_tsvettsikh@epam.com>
2020-06-17 14:11:53 +0700

    Refactor

--- After/DataAccess.MsSql/MsSqlEfFunctions.cs
+++ After/DataAccess.MsSql/MsSqlEfFunctions.cs
@@ -1,8 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
-using System.Linq;
+﻿using System.Linq;
 using System.Linq.Expressions;
 using Infrastructure.Interfaces.DataAccess;
-using Microsoft.EntityFrameworkCore;
 
 namespace DataAccess.MsSql
 {
@@ -10,6 +8,7 @@ namespace DataAccess.MsSql
     {
         public Expression Like(MethodCallExpression node)
         {
+            //Postgres
             //var method = typeof(DbFunctionsExtensions).GetMethods()
             //    .Where(x => x.Name == "Like")
             //    .First();
@@ -17,30 +16,31 @@ namespace DataAccess.MsSql
             //var res = Expression.Call(null, method, ext, node.Arguments[0], node.Arguments[1]);
             //return res;
 
+            //MS SQL
             var str = node.Arguments.First();
             var pattern = (string) ((ConstantExpression) node.Arguments.Last()).Value;
             var toLowerMethod = typeof(string).GetMethods().First(x => x.Name == "ToLower");
-            string methodName;
-            switch (pattern)
-            {
-                case var p when p.StartsWith('%') && p.EndsWith('%'):
-                    methodName = "Contains";
-                    break;
-                case var p when p.StartsWith('%'):
-                    methodName = "EndsWith";
-                    break;
-                case var p when p.EndsWith('%'):
-                    methodName = "StartsWith";
-                    break;
-                default: 
-                    methodName = "Equals";
-                    break;
-            }
-            var endsWithMethod = typeof(string).GetMethods().First(x => x.Name == methodName);
+            string stringMethodName = GetMethodName(pattern);
+            var endsWithMethod = typeof(string).GetMethods().First(x => x.Name == stringMethodName);
             var res = Expression.Call(str, toLowerMethod);
             var constExpr = Expression.Constant(pattern.Trim('%'));
             res = Expression.Call(res, endsWithMethod, constExpr);
             return res;
+
+            string GetMethodName(string ptrn)
+            {
+                switch (ptrn)
+                {
+                    case var p when p.StartsWith('%') && p.EndsWith('%'):
+                        return "Contains";
+                    case var p when p.StartsWith('%'):
+                        return "EndsWith";
+                    case var p when p.EndsWith('%'):
+                        return "StartsWith";
+                    default:
+                        return "Equals";
+                }
+            }
         }
     }
 }
